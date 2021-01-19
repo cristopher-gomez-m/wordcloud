10f44fca1ef139716d65a1c6e15828430e0c316d
cristopher-gomez-m <58451312+cristopher-gomez-m@users.noreply.github.com>
2021-01-19 10:26:24 -0500

    agregada la funcion en/es

--- word_cloud.py
+++ word_cloud.py
@@ -7,38 +7,77 @@ from wordcloud import WordCloud, ImageColorGenerator
 from PIL import Image
 import matplotlib.pyplot as plt
 
-ID= input("Ingrese el id del usuario que desea hacer la imagen de nubes de palabras: ")
+idioma=input("Ingrese es si el ID es de stackoverflow español o en si el ID de stackoverflow ingles: ")
 
+if idioma == "en":
+    ID= input("Ingrese el id del usuario que desea hacer la imagen de nubes de palabras: ")
+    page = requests.get('https://stackoverflow.com/users/{}/?tab=tags'.format(ID))   
+    page2= requests.get('https://stackoverflow.com/users/{}/?tab=tags'.format(ID)) 
+    soup = BeautifulSoup(page.content, 'html.parser')
+    soup2 = BeautifulSoup(page2.content, 'html.parser')
 
-page = requests.get('https://es.stackoverflow.com/users/{}/?tab=tags'.format(ID))  
-page2= requests.get('https://es.stackoverflow.com/users/{}/?tab=tags'.format(ID))
+    palabras = soup.find_all('a', class_='post-tag')
+    numeros = soup.find_all(class_='item-multiplier-count' )
 
-soup = BeautifulSoup(page.content, 'html.parser')
-soup2 = BeautifulSoup(page2.content, 'html.parser')
+    pb = list()
 
-palabras = soup.find_all('a', class_='post-tag')
-numeros = soup.find_all(class_='item-multiplier-count' )
+    num= list()
 
-pb = list()
-
-num= list()
-
-for i in numeros:
-    num.append(i.text)
-
-
-for j in palabras:
-    pb.append(j.text)
-
-lista_numeros= [int(x) for x in num]
-
-diccionario= dict(zip(pb,lista_numeros))
-print(diccionario)
+    for i in numeros:
+        num.append(i.text)
 
 
 
+    for j in palabras:
+        pb.append(j.text)
 
-word_cloud = WordCloud(height=800, width=800, background_color='white',max_words=150, min_font_size=5).generate_from_frequencies(diccionario) 
-plt.imshow(word_cloud, interpolation='bilinear')
-plt.axis("off")
-plt.show()
+   
+
+    lista_numeros= [int(x) for x in num]
+
+    diccionario= dict(zip(pb,lista_numeros))
+
+    word_cloud = WordCloud(height=800, width=800, background_color='white',max_words=150, min_font_size=5).generate_from_frequencies(diccionario) 
+    plt.imshow(word_cloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
+
+    
+elif idioma == "es":
+    ID= input("Ingrese el id del usuario que desea hacer la imagen de nubes de palabras: ")
+    page = requests.get('https://es.stackoverflow.com/users/{}/?tab=tags'.format(ID))   
+    page2= requests.get('https://es.stackoverflow.com/users/{}/?tab=tags'.format(ID)) 
+    soup = BeautifulSoup(page.content, 'html.parser')
+    soup2 = BeautifulSoup(page2.content, 'html.parser')
+
+    palabras = soup.find_all('a', class_='post-tag')
+    numeros = soup.find_all(class_='item-multiplier-count' )
+
+    pb = list()
+
+    num= list()
+
+    for i in numeros:
+        num.append(i.text)
+
+
+
+    for j in palabras:
+        pb.append(j.text)
+
+   
+
+    lista_numeros= [int(x) for x in num]
+
+    diccionario= dict(zip(pb,lista_numeros))
+    print(diccionario)
+    
+    
+
+
+
+
+    word_cloud = WordCloud(height=800, width=800, background_color='white',max_words=150, min_font_size=5).generate_from_frequencies(diccionario) 
+    plt.imshow(word_cloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
