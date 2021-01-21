2248a33a8ec6b06a8f6096c8e3d4034a79292027
cristopher-gomez-m <58451312+cristopher-gomez-m@users.noreply.github.com>
2021-01-21 08:52:22 -0500

    removida la funcionalidad de nubes de palabras para stackoverflow en ingles

--- word_cloud.py
+++ word_cloud.py
@@ -4,31 +4,12 @@ import requests
 from wordcloud import WordCloud, ImageColorGenerator
 import matplotlib.pyplot as plt
 
-#Hacemos dos funciones,de acuerdo a la pagina a la que se vaya a hacer la petición
-def busqueda_es():
-    ID= input("Ingrese el id del usuario que desea hacer la imagen de nubes de palabras: ")
-    
-    page = requests.get('https://es.stackoverflow.com/users/{}/?tab=tags'.format(ID))   
-    page2= requests.get('https://es.stackoverflow.com/users/{}/?tab=tags'.format(ID)) 
-    return page,page2
-
-def busqueda_en():
-    ID= input("Ingrese el id del usuario que desea hacer la imagen de nubes de palabras: ")
-    page = requests.get('https://stackoverflow.com/users/{}/?tab=tags'.format(ID))   
-    page2= requests.get('https://stackoverflow.com/users/{}/?tab=tags'.format(ID)) 
-    return page,page2
-
 if __name__ == "__main__":
-    idioma=input("Ingrese es si el ID es de stackoverflow español o en si el ID de stackoverflow ingles: ")
-    if idioma == "en":
-        page,page2=busqueda_en()
-    
-    
-    elif idioma == "es":
-        page,page2=busqueda_es()
-    #Ya sea español o ingles,se utiliza el mismo nombre para reducir las lineas de código
 
 
+    ID= input("Ingrese el id del usuario que desea hacer la imagen de nubes de palabras: ")
+    page = requests.get('https://es.stackoverflow.com/users/{}/?tab=tags'.format(ID))   
+    page2= requests.get('https://es.stackoverflow.com/users/{}/?tab=tags'.format(ID))
     soup = BeautifulSoup(page.content, 'html.parser')
     soup2 = BeautifulSoup(page2.content, 'html.parser')
 
@@ -53,9 +34,11 @@ if __name__ == "__main__":
 
     diccionario= dict(zip(pb,lista_numeros))
     #Se comprime en un diccionario para darle una llave y un valor para pasarlo al wordcloud
+
+
     word_cloud = WordCloud(height=800, width=800, background_color='white',max_words=150, min_font_size=5).generate_from_frequencies(diccionario) 
+    #Se comprime en un plano 2d y se oculta el plano cartersiano
     plt.imshow(word_cloud, interpolation='bilinear')
     plt.axis("off")
-    #Se comprime en un plano 2d y se oculta el plano cartersiano
     plt.show()
     
